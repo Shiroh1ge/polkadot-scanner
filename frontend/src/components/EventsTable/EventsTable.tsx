b8de070736514f22ff24162a3eeb28f8feb4af66
Shiroh1ge <hakera_bg@msn.com>
2022-06-07 10:26:44 +0300

    display data in table - wip

--- frontend/src/components/EventsTable/EventsTable.tsx
+++ frontend/src/components/EventsTable/EventsTable.tsx
@@ -1,89 +1,110 @@
+import { withStyles } from '@mui/styles';
 import { GridColumns } from '@mui/x-data-grid/models/colDef/gridColDef';
 import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridRowHeightParams } from '@mui/x-data-grid';
 
 
-interface Data {
-    id: number;
-    blockNumber: number;
-    eventName: string;
-    eventArguments: string;
+export interface BlockEvent {
+  id: number;
+  blockNumber: number;
+  eventName: string;
+  eventArguments: string;
 }
 
-const rows: Data[] = [
-    {
-        id: 0,
-        blockNumber: 1,
-        eventName: 'test_1',
-        eventArguments: 'test, test'
+const rows: BlockEvent[] = [
+  {
+    id: 0,
+    blockNumber: 1,
+    eventName: 'test_1',
+    eventArguments: 'test, test'
+  },
+  {
+    id: 1,
+    blockNumber: 2,
+    eventName: 'test_2',
+    eventArguments: 'test_2, test_2'
+  },
+  {
+    id: 2,
+    blockNumber: 1,
+    eventName: 'test_1',
+    eventArguments: 'test, test'
+  },
+  {
+    id: 3,
+    blockNumber: 2,
+    eventName: 'test_2',
+    eventArguments: 'test_2, test_2'
+  },
+  {
+    id: 4,
+    blockNumber: 1,
+    eventName: 'test_1',
+    eventArguments: 'test, test'
+  },
+  {
+    id: 5,
+    blockNumber: 2,
+    eventName: 'test_2',
+    eventArguments: 'test_2, test_2'
+  },
+  {
+    id: 6,
+    blockNumber: 1,
+    eventName: 'test_1',
+    eventArguments: 'test, test'
+  },
+  {
+    id: 7,
+    blockNumber: 2,
+    eventName: 'test_2',
+    eventArguments: 'test_2, test_2'
+  }
+];
+
+const columns: GridColumns<BlockEvent> = [
+  { field: 'blockNumber', headerName: 'Block #', width: 120 },
+  { field: 'eventName', headerName: 'Event Name', width: 250 },
+  { field: 'eventArguments', headerName: 'Event Arguments', width: 350 }
+];
+
+const StyledDataGrid = withStyles({
+  root: {
+    '& .MuiDataGrid-renderingZone': {
+      maxHeight: 'none !important'
     },
-    {
-        id: 1,
-        blockNumber: 2,
-        eventName: 'test_2',
-        eventArguments: 'test_2, test_2'
-    },
-    {
-        id: 2,
-        blockNumber: 1,
-        eventName: 'test_1',
-        eventArguments: 'test, test'
-    },
-    {
-        id: 3,
-        blockNumber: 2,
-        eventName: 'test_2',
-        eventArguments: 'test_2, test_2'
-    },
-    {
-        id: 4,
-        blockNumber: 1,
-        eventName: 'test_1',
-        eventArguments: 'test, test'
-    },
-    {
-        id: 5,
-        blockNumber: 2,
-        eventName: 'test_2',
-        eventArguments: 'test_2, test_2'
-    },
-    {
-        id: 6,
-        blockNumber: 1,
-        eventName: 'test_1',
-        eventArguments: 'test, test'
-    },
-    {
-        id: 7,
-        blockNumber: 2,
-        eventName: 'test_2',
-        eventArguments: 'test_2, test_2'
+    '& .MuiDataGrid-cellContent': {
+      lineHeight: 'unset !important',
+      maxHeight: 'none !important',
+      whiteSpace: 'normal',
+      'overflowWrap': 'break-word'
+
+},
+    '& .MuiDataGrid-row': {
+      maxHeight: 'none !important'
     }
-];
-
-const columns: GridColumns<Data> = [
-    { field: 'blockNumber', headerName: 'Block #',  width: 120},
-    { field: 'eventName', headerName: 'Event Name', width: 250 },
-    { field: 'eventArguments', headerName: 'Event Arguments', width: 250  },
-];
+  }
+})(DataGrid);
 
 
-const EventsTable = () => {
+const EventsTable = ({ data }: { data: BlockEvent[] }) => {
 
-    return (
-        <div className="w-full h-full">
-            <DataGrid
-                className="w-full h-full"
-                rows={rows}
-                columns={columns}
-                pageSize={10}
-                rowsPerPageOptions={[5]}
-                checkboxSelection={false}
-                disableSelectionOnClick={true}
-                autoHeight={true}
-            />
-        </div>
-    );
+  return (
+    <div className="w-full h-full">
+      <StyledDataGrid
+        className="w-full h-full"
+        rows={data}
+        // @ts-ignore the styled data grid doesn't recognize the column generic type correctly
+        columns={columns}
+        pageSize={10}
+        rowHeight={55}
+        rowsPerPageOptions={[5]}
+        checkboxSelection={false}
+        disableSelectionOnClick={true}
+        autoHeight={true}
+      />
+    </div>
+  );
 };
 
 export default EventsTable;
--- frontend/src/pages/BlockchainScanner/BlockchainScanner.tsx
+++ frontend/src/pages/BlockchainScanner/BlockchainScanner.tsx
@@ -2,7 +2,7 @@ import { Button, FormControl, FormHelperText, TextField } from '@mui/material';
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import React, { useEffect, useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import EventsTable from '../../components/EventsTable/EventsTable';
+import EventsTable, { BlockEvent } from '../../components/EventsTable/EventsTable';
 import { WSS_URL_REGEX } from '../../constants/validation.constants';
 import { createPolkadotApi, usePolkadotApi } from '../../hooks/usePolkadotApi.hook';
 
@@ -16,28 +16,69 @@ const defaultValues: FormValues = { startBlock: undefined, endBlock: undefined, 
 
 const BlockchainScanner = (): JSX.Element => {
     const { register, handleSubmit, formState, getValues, setValue, control } = useForm<FormValues>({ defaultValues, mode: 'onChange' });
-    const endpoint = useRef<string>(defaultValues.endpoint);
+    const [ blockEvents, setBlockEvents ] = useState<BlockEvent[]>([]);
+    const currentEndpoint = useRef<string>(defaultValues.endpoint);
 
     const api = useRef<ApiPromise>();
 
     useEffect(() => {
       const setInitialEndBlockValue = async () => {
-        api.current = await createPolkadotApi(defaultValues.endpoint);
-        const latestBlock = await api.current.rpc.chain.getHeader();
-        setValue('endBlock', latestBlock?.number.toNumber());
+        api.current = await createPolkadotApi(currentEndpoint.current);
+        const latestBlock = await api.current.rpc.chain.getBlock();
+        setValue('endBlock', latestBlock?.block.header.number.toNumber());
       };
 
       setInitialEndBlockValue();
 
     }, []);
 
-    const scan = async (values: FormValues): Promise<void> => {
-      endpoint.current = values.endpoint;
-      console.log('api.current', api);
+    const scan = async ({ startBlock, endBlock, endpoint }: FormValues): Promise<void> => {
+      // we only create a new api if the endpoint has changed
+      if (endpoint !== currentEndpoint.current) {
+        currentEndpoint.current = endpoint;
+        api.current = await createPolkadotApi(currentEndpoint.current);
+      }
+
+
+      const latestBlock = await api.current!.rpc.chain.getBlock();
+      const hash = await api.current!.rpc.chain.getBlockHash(10627934);
+      const block = await api.current!.rpc.chain.getBlock(hash);
+
+      const apiAt = await api.current!.at(hash);
+      await apiAt.query.system.events();
+      const allRecords = await apiAt.query.system.events((events: { event: any; phase: any; }[]) => {
+        console.log(`\nReceived ${events.length} events:`);
+
+        const newBlockEvents: BlockEvent[] = [];
+
+        events.forEach((record: { event: any; phase: any; }, i) => {
+          const { event, phase } = record;
+          const types = event.typeDef;
+          const blockEvent: BlockEvent = {
+            id: i,
+            blockNumber: 10627934,
+            eventName: event.method,
+            eventArguments: event.data.toString()
+          }
+
+          newBlockEvents.push(blockEvent);
+
+
+          // Show what we are busy with
+          console.log(`test \t${event.section}:${event.method}`);
+
+          // Loop through each of the parameters, displaying the type and data
+          event.data.forEach((data: { toString: () => any; }, index: string | number) => {
+            console.log(`\t\t\t${types[index].type}: ${data.toString()}`);
+          });
+        });
+
+        setBlockEvents([...blockEvents, ...newBlockEvents]);
+
+      });
     };
 
     const onSubmit = () => {
-      console.log('getValues()', getValues());
       if (formState.isValid) {
         scan(getValues());
       }
@@ -99,7 +140,7 @@ const BlockchainScanner = (): JSX.Element => {
         </div>
 
         <div className="flex h-full w-full">
-          <EventsTable/>
+          <EventsTable data={blockEvents}/>
         </div>
       </div>
     );
